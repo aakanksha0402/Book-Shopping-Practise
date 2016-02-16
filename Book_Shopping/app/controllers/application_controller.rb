e73d868f9f610d3eacb91628c586b25e89e6cbd2
aakanksha0402 <aakanksha0402@gmail.com>
2016-02-16 16:59:43 +0530

    Update application_controller.rb

--- Book_Shopping/app/controllers/application_controller.rb
+++ Book_Shopping/app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  private
+  
   def current_admin
     @admin_details ||= Administrator.find(session[:admin_id]) if session[:admin_id]
   end
