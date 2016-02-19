5942f4033363d839f781542fc4407e2279956d57
aakanksha0402 <aakanksha0402@gmail.com>
2016-02-19 10:36:56 +0530

    Removed active admin

--- Book_Shopping_ActiveAdmin/app/controllers/concerns/create_cart.rb
+++
@@ -1,9 +0,0 @@
-module CreateCart extend ActiveSupport::Concern
-private
-def cart_assign
-  @cart = Cart.find(session[:cart_id])
-  rescue ActiveRecord::RecordNotFound
-    @cart  = Cart.create
-    session[:cart_id] = @cart.id
-end
-end
--- Book_Shopping_ActiveAdmin/app/controllers/store_controller.rb
+++
@@ -1,8 +0,0 @@
-class StoreController < ApplicationController
-include CreateCart
-before_action :cart_assign
-  def index
-    @product = Product.order(:title)
-    @add_product = Product.new
-  end
-end
